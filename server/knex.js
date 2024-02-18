08b4c9deec009f420fe4434633fbdc985f303d75
ShizuokaTerrier <matt@squareoneJapan.com>
2024-02-18 17:25:57 +0900

    fix:knex edit

--- server/knex.js
+++ server/knex.js
@@ -2,4 +2,4 @@ require('dotenv').config({path:"./env"});
 const knex = require('knex');
 const config = require('./knexfile')
 
-module.exports = process.env.NODE_ENV === 'production' ? knex(config['production']) : knex(config['development']);
+module.exports = knex(config.production);
